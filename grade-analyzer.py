6d7ee6283243e2fe7bd8fd23239a1bc18537bba5
Christian Tannoury <tsmchris42@gmail.com>
2025-06-19 18:43:03 +0300

    Fifth Commit: Adjusting Bugs

--- grade-analyzer.py
+++ grade-analyzer.py
@@ -24,7 +24,7 @@ def display_student_name(): # O(1)
         print("Grade:", student_grades[student]) # O(1)
 
 # Calling the function
-display_student_name() # O(1)
+display_student_name() 
 
 # Overall complexity of the function: O(n) 
 def get_avg_grade(grades, count_students): # O(1)
@@ -33,21 +33,40 @@ def get_avg_grade(grades, count_students): # O(1)
         sum += student_grade # O(1)
     print("\nAverage Grade of the Class:", sum / count_students ) # O(1)
 
+# Method 2
+# Averge Grade of the Class (Recursive Function)
+# Overall complexity of the function: O(n) 
+# def get_avg_grade(grades, count_students, sum=0): # O(1)
+#     if  len(grades) == 0: # Base Case &  O(1)
+#         print("\nAverage Grade of the Class:", sum / count_students ) # O(1)
+#         return # O(1)
+#     sum += grades[0] # O(1)
+#     grades.pop(0) # O(1)
+#     get_avg_grade(grades, count_students, sum) # O(n)
+
+
+
 # Calling the function
-get_avg_grade(student_grades, number_students) # O(1)
+get_avg_grade(student_grades, number_students) 
 
 # Overall complexity of the function: O(n) 
-def get_highest_grade(grades, names, index=0, max_index=0): # O(1)
-    if index == len(grades): # O(1)
+def get_highest_grade(grades, names, index=0, max_grade=0, max_indices=0): # O(1)
+    if index == len(grades): # Base Case & O(1)
         print("\n**** Highest Grade ****") # O(1)
-        print("Name:", names[max_index]) # O(1)
-        print("Grade:", grades[max_index]) # O(1)
-        return # O(1)
+        for i in max_indices:  # O(m) 
+            print("Name:", names[i])  # O(1)
+            print("Grade:", grades[i])  # O(1)
+        return  # O(1)
     
-    if grades[index] > grades[max_index]: # O(1)
-        max_index = index # O(1)
+    current_grade = grades[index]
+    
+    if current_grade > max_grade: # O(1)
+        max_grade = current_grade # O(1)
+        max_indices = [index] # O(1)
+    elif current_grade == max_grade: # O(1)
+        max_indices.append(index) # O(1)
 
-    return get_highest_grade(grades, names, index + 1, max_index) # O(n)
+    return get_highest_grade(grades, names, index + 1, max_grade, max_indices) # O(n)
 
 # Calling the function
-get_highest_grade(student_grades, student_names)
+get_highest_grade(student_grades, student_names) 
