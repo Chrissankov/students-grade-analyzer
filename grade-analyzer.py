6525756d2e5ebf5b05a481081eb93a72fffa7c74
Christian Tannoury <tsmchris42@gmail.com>
2025-06-19 14:59:32 +0300

    Second Commit: Displaying Student Summary

--- grade-analyzer.py
+++ grade-analyzer.py
@@ -1,25 +1,26 @@
-number_students: int = int(input("Number of Students: "))  # O(1)
-student_names = []  # O(1)
-student_grades = []  # O(1)
+number_students: int = int(input("Number of Students: ")) # O(1)
+student_names = [] # O(1)
+student_grades = [] # O(1)
 
-# Overall complexity of the loop: O(n) where n = number of students
-for student in range(number_students):  # O(n)
-    student_name: str = input(str(student + 1) + " - Student Name: ")  # O(1)
+# Overall complexity of the loop: O(n)
+for student in range(number_students): # O(n)
+    student_name: str = input(str(student + 1) + " - Student Name: ") # O(1)
 
-    # In worst case, user keeps entering invalid grades m times: O(m)
+    # In worst case user keeps entering invalid grades m times: O(m)
     while True:  
-        student_grade: int = int(input(str(student + 1) + " - Student Grade: "))  # O(1)
+        student_grade: int = int(input(str(student + 1) + " - Student Grade: ")) # O(1)
         if student_grade >= 0 and student_grade <= 100: # O(1) 
-            break  # O(1)
-        print("Grade must be between 0 and 100 inclusively.")  # O(1)
+            break # O(1)
+        print("Grade must be between 0 and 100 inclusively.") # O(1)
 
-    student_names.append(student_name)  # O(1)
-    student_grades.append(student_grade)  # O(1)
+    student_names.append(student_name) # O(1)
+    student_grades.append(student_grade) # O(1)
 
-def display_student_name(): 
-    for student in range(number_students):
-        print("\n**** Student" , str(student + 1) , "****")
-        print("Name:", student_names[student])
-        print("Grade:", student_grades[student])
+# Overall complexity of the loop: O(n) 
+def display_student_name(): # O(n)
+    for student in range(number_students): # O(n)
+        print("\n**** Student" , str(student + 1) , "****") # O(1)
+        print("Name:", student_names[student]) # O(1)
+        print("Grade:", student_grades[student]) # O(1)
 
-display_student_name()
+display_student_name() # O(1)
